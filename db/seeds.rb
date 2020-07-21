661b4d3c23695835cf4c07ed23ef37bcdb01b751
Taevon Jordan <nude.plus@MacBook-Pro.home>
2020-07-21 16:12:59 -0400

    songs edit form

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.destroy_all
+Genre.destroy_all
+Song.destroy_all
+
+Artist.create(name: "David", bio: "i like making music.")
+Artist.create(name: "John", bio: "i make song.")
+Artist.create(name: "eric", bio: "song is good.")
+
+Genre.create(name: "Hip-Hop")
+Genre.create(name: "R&B")
+Genre.create(name: "Soul")
+
+20.times do
+  Song.create(name: "I made this song", artist_id: Artist.all.sample.id, genre_id: Genre.all.sample.id)
+end
